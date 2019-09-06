67f57add508b16dd7751bba9e43d874d8d84e0ea
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-09-05 19:09:13 -0500

    initialize photo gallery

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,24 @@ require('@/assets/styles/reset.css');
 require('@/assets/styles/main.css');
 
 import photos from './data/photos';
+import PhotoGrid from './components/PhotoGrid';
 
 export default {
   name: 'app',
-  // data() {
-  //   return {
-      
-  //   }
-  // }
+  data() {
+    return {
+      photos: photos,
+    }
+  },
+  components: {
+    PhotoGrid
+  },
 }
 </script>
 
 <template>
   <div id="app">
     <h1>test</h1>
+    <PhotoGrid v-bind:photos="photos"></PhotoGrid>
   </div>
 </template>
---
+++ src/components/PhotoGrid.vue
@@ -0,0 +1,45 @@
+<script>
+import Photoswipe from 'vue-pswipe';
+import Vue from 'vue';
+const options = {
+            bgOpacity: 0.8,
+            captionEl: false,
+            tapToClose: true,
+            shareEl: false,
+            fullscreenEl: false,
+            preload: [0,1],
+        };
+Vue.use(Photoswipe, options);
+
+export default {
+    name: 'PhotoGrid',
+    props: {
+        photos: {
+            type: Array,
+            required: true,
+            default: () => [],
+        },
+        template: {
+            type: String,
+            required: false,
+            default: 'photoswipe-gallery',
+        },
+    },
+}
+</script>
+
+<template>
+    <div>
+        <Photoswipe v-bind:class="template">
+            <div class="photoswipe-image"
+                v-for="(src, index) in photos"
+                :key="index">
+                <img
+                    :data-src="src"
+                    :data-pswp-src="src"
+                    class="lazy"
+                />
+            </div>
+        </Photoswipe>
+    </div>
+</template>
