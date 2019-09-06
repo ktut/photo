22d3c8c30b339948db213e79f527ed97ad64f45a
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-09-05 19:00:18 -0500

    setup styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+require('@/assets/styles/main.css');
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // data() {
+  //   return {
+      
+  //   }
+  // }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id="app">
+    <h1>test</h1>
+  </div>
+</template>
