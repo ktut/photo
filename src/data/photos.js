0e248140df3e3472e9d0bc14eeb253a21b290539
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-09-06 18:57:38 -0500

    need to get anim working again for moving

--- src/data/photos.js
+++ src/data/photos.js
@@ -2,38 +2,38 @@ export default [
     {   
         id: 0,
         url: 'https://ktut.github.io/portfolio/assets/photo/ruffalo-1.jpg',
-        type: 'people',
-        style: '' 
+        type: 'places',
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 1,
         url: 'https://ktut.github.io/portfolio/assets/photo/WhoWatchR-SLH-122114.01-2.JPG',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 2,
         url: 'https://ktut.github.io/portfolio/assets/photo/will-1100.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 3,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG_0038-2.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {    
         id: 4,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG_0905.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 5,
         url: 'https://ktut.github.io/portfolio/assets/photo/1100-westwick.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 6,
@@ -45,36 +45,36 @@ export default [
         id: 7,
         url: 'https://ktut.github.io/portfolio/assets/photo/00-aless.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 8,
         url: 'https://ktut.github.io/portfolio/assets/photo/miles-49-RAMZI-DREESSEN.jpg',
-        type: 'people',
-        style: '' 
+        type: 'places',
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 9,
         url: 'https://ktut.github.io/portfolio/assets/photo/10572022_3409851362463_3147035676682537141_o.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {    
         id: 10,
         url: 'https://ktut.github.io/portfolio/assets/photo/E8A2046-WEB.jpg',
         type: 'people',
-        style: '' 
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 11,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG-365.jpg',
-        type: 'people',
-        style: '' 
+        type: 'places',
+        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 12,
         url: 'https://ktut.github.io/portfolio/assets/photo/lefty-3.jpg',
-        type: 'people',
-        style: '' 
+        type: 'places',
+        style: 'grid-row-end: span 2;' 
     },
 ];
