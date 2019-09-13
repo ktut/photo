56a9e7eeec7ed8744c90458badebe0fcd46419bd
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-09-12 19:12:44 -0500

    advance design

--- src/data/people.js
+++ src/data/people.js
@@ -6,50 +6,37 @@ export default [
         id: 0,
         url: 'https://ktut.github.io/portfolio/assets/photo/ruffalo-1.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 1,
         url: 'https://ktut.github.io/portfolio/assets/photo/WhoWatchR-SLH-122114.01-2.JPG',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 2,
         url: 'https://ktut.github.io/portfolio/assets/photo/will-1100.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 4' 
+        class: 'large',
     },
     {   
         id: 3,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG_0038-2.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {    
         id: 4,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG_0905.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 5,
         url: 'https://ktut.github.io/portfolio/assets/photo/1100-westwick.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 6,
         url: 'https://ktut.github.io/portfolio/assets/photo/00-aless.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 7,
@@ -62,35 +49,26 @@ export default [
         id: 8,
         url: 'https://ktut.github.io/portfolio/assets/photo/miles-49-RAMZI-DREESSEN.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 9,
         url: 'https://ktut.github.io/portfolio/assets/photo/10572022_3409851362463_3147035676682537141_o.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {    
         id: 10,
         url: 'https://ktut.github.io/portfolio/assets/photo/E8A2046-WEB.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
     {   
         id: 11,
         url: 'https://ktut.github.io/portfolio/assets/photo/IMG-365.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 4' 
+        class: 'large',
     },
     {   
         id: 12,
         url: 'https://ktut.github.io/portfolio/assets/photo/lefty-3.jpg',
         type: 'people',
-        class: 'testClass',
-        style: 'grid-row-end: span 2;' 
     },
 ];
--- src/data/places.js
+++ src/data/places.js
@@ -6,49 +6,47 @@ export default [
         id: 13,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1435362490548-CRK2AM9YDO27BIOSA59D/Inside-3.jpg?format=1500w&content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
     {   
         id: 14,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1435362405927-H0DLEWDUPYQOZOAIIR87/image-asset.jpeg?format=1500w&content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
     {   
         id: 15,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1435362490522-LITRMDL3D50KBUACH9UQ/Inside-2-SCREEN-EDIT.jpg?format=1500w&content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
     {   
         id: 16,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1435362748340-JMC0KLNYSCFNI6HQNTVN/Inside-14.jpg?format=1000w&content-type=image%2Fjpeg',
-        type: 'places',
-        class: 'testClass',
-        style: 'grid-row-end: span 2; background-position: 50% 100%;' 
+        type: 'places', 
     },
     {   
         id: 17,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1459201804015-SZ1Y4Y8DNAESPIJ3WEKG/avant-interior.jpg?format=1500w&content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
     {   
         id: 18,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1537825070094-6FENWTYMSFD1K7NORT7D/mahalo-2.jpg?format=1500w&content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
     {   
         id: 19,
         url: 'https://images.squarespace-cdn.com/content/54318df1e4b067871dac78e3/1537825072539-WO57FZQ1BVIXF3NIYU8T/outside.jpg?content-type=image%2Fjpeg',
         type: 'places',
-        class: 'testClass',
-        style: 'grid-column-end: span 2; grid-row-end: span 2; background-position: 50% 50%;' 
+        class: 'wide',
+        style: 'background-position: 50% 50%;' 
     },
 ];
