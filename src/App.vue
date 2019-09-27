fb1b8589e232edcd9d720adad83b0ec4e073caca
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-09-26 20:33:30 -0500

    make top nav icons on mobile

--- src/App.vue
+++ src/App.vue
@@ -25,21 +25,23 @@ export default {
 
 <template>
   <div id="app">
-    <nav style="padding-top: 15px; justify-content: space-between;">
+    <nav style="padding-top: 20px; padding-bottom: 15px; justify-content: space-between;">
       <div class="title-link">
         <h1 class="title">
           RKDVISUALS
         </h1>
       </div>
-      <router-link :to="{name: 'Photo'}" style="text-decoration: none !important;" class="nav-button">
-        Photo
-      </router-link>
-      <router-link :to="{name: 'Video'}" style="text-decoration: none !important;" class="nav-button">
-        Video
-      </router-link>
-      <router-link :to="{name: 'Contact'}" class="nav-button">
-        Contact
-      </router-link>
+      <div style="display: flex;">
+        <router-link :to="{name: 'Photo'}" style="text-decoration: none !important;" class="nav-button photo">
+          Photo
+        </router-link>
+        <router-link :to="{name: 'Video'}" style="text-decoration: none !important;" class="nav-button video">
+          Video
+        </router-link>
+        <router-link :to="{name: 'Contact'}" class="nav-button info">
+          Contact
+        </router-link>
+      </div>
     </nav>
     <main>
       <transition v-bind:name="transitionName">
