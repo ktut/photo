251e55795d3c63d4515bb7f05444e5dc1fbb7973
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-10-04 17:44:06 -0500

    add key for videos data

--- src/data/videos.js
+++ src/data/videos.js
@@ -3,24 +3,31 @@
 
 export default [
     {
+        id: 1,
         url: 'https://www.youtube.com/embed/7nJw3GBk9TA',
     },
     {
+        id: 2,
         url: 'https://www.youtube.com/embed/5_E0grmgexs',
     },
     {
+        id: 3,
         url: 'https://www.youtube.com/embed/krsBy0WHvRo',
     },
     {
+        id: 4,
         url: 'https://www.youtube.com/embed/-phcnAafvBw',
     },
     {
+        id: 5,
         url: 'https://www.youtube.com/embed/9oMq5amtjdQ',
     },
     {
+        id: 6,
         url: 'https://www.youtube.com/embed/yQH8lrafeqI',
     },
     {
+        id: 7,
         url: 'https://www.youtube.com/embed/FAPzes8rkmk',
     },
 ];
